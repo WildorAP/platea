ad590625ea265f5eb6d1feffc4a2efea7308955e
plateahub <wildorapaza95@gmail.com>
2024-09-10 20:54:26 -0500

    Cambio4

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Definir las tasas de cambio para compra y venta
 // Definir los precios de venta
 const precios = {
-    producto1: 0.999, //BUY USDT - USD
+    producto1: 0.995, //BUY USDT - USD
     producto2: 3.82,    //BUY USDT - PEN
-    producto3: 0.989,    //SELL USDT - USD
+    producto3: 0.985,    //SELL USDT - USD
     producto4: 3.78,     //SELL USDT - PEN
 };
 
